1dd30d35da098716dc031e0ac3e845e4f742aef1
Luca Barco <s276072@studenti.polito.it>
2020-12-31 15:50:20 +0100

    general fix

--- Q2-GrailParallelImplementation/main.cpp
+++ Q2-GrailParallelImplementation/main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char** argv) {
 		return 1;
 	}*/
 	TCHAR name[100] = { _T("myGraph") };
-	TCHAR graphInput[1000] = { _T("myGraph.gra") };
-	TCHAR queryInput[1000] = { _T("myGraph.que") };
+	TCHAR graphInput[10000];
+	TCHAR queryInput[10000];
 	options mem_option;
 	graphOptions graph_option;
 	int choice, labels;
@@ -41,8 +41,10 @@ int main(int argc, char** argv) {
 
 	printf("We have developed two possible implementations of the GRAIL algorithm:\n\n \t- In the first one, the graph is entirely loaded in main memory, as an adjacency list.\n\t\t Some memory overloads could happen for big and/or dense graphs, depending on your machine\n\n\t-In the second one, the graph is stored in a binary file, allowing also very big and/or dense graphs.\n\t\tIt may take longer, because of the disk overhead.\n\n");
 	do {
+		swprintf_s(graphInput, _T("myGraph.gra"));
+		swprintf_s(queryInput, _T("myGraph.que"));
 		do {
-			printf("Make your choice: \n\t\t- 0: load the in-memory version \n\t\t- 1: load the in-file version\n\t\t- 2: exit\n ----> ");
+			printf("Choose the Grail version: \n\n\t\t- 0: load the in-memory version \n\t\t- 1: load the in-file version\n\t\t- 2: exit\n\n----> Make your choice: ");
 			scanf_s("%d", &mem_option);
 
 			switch (mem_option) {
@@ -61,11 +63,12 @@ int main(int argc, char** argv) {
 		} while (mem_option != memory && mem_option != file);
 
 		do {
-			printf("\nSelect your input graph file:");
+			printf("\nSelect your input graph file:\n");
 			printf("\n\t0- Small dense benchmark");
 			printf("\n\t1- Small sparse benchmark");
 			printf("\n\t2- Large benchmark");
 			printf("\n\t3- Generate your own file\n");
+			printf("\n\n----> Make your choice: ");
 			scanf_s("%d", &graph_option);
 
 			switch (graph_option) {
@@ -73,12 +76,13 @@ int main(int argc, char** argv) {
 				printf("\nOk,let's use one of small dense real graphs\n");
 
 				do {
-					printf("\nSelect your input graph file:");
+					printf("\nSelect your input graph file:\n");
 					printf("\n\t0- arXiv_sub_6000-1");
 					printf("\n\t1- citeseer_sub_10720");
 					printf("\n\t2- go_sub_6793");
 					printf("\n\t3- pubmed_sub_9000-1");
 					printf("\n\t4- yago_sub_6642\n");
+					printf("\n\n----> Make your choice: ");
 					scanf_s("%d", &choice);
 
 					switch (choice) {
@@ -99,8 +103,8 @@ int main(int argc, char** argv) {
 						swprintf_s(queryInput, _T("./benchmark/small_dense_real/pubmed_sub_9000-1.que"));
 						break;
 					case 4:
-						swprintf_s(graphInput, _T("./benchmark/small_dense_real/yago_sub_664.gra"));
-						swprintf_s(queryInput, _T("./benchmark/small_dense_real/yago_sub_664.que"));
+						swprintf_s(graphInput, _T("./benchmark/small_dense_real/yago_sub_6642.gra"));
+						swprintf_s(queryInput, _T("./benchmark/small_dense_real/yago_sub_6642.que"));
 						break;
 					default:
 						printf("\nSorry, I cannot understand what you want to do. Retry\n");
@@ -112,7 +116,7 @@ int main(int argc, char** argv) {
 				printf("\nOk,let's use one of small sparse real graphs\n");
 
 				do {
-					printf("\nSelect your input graph file:");
+					printf("\nSelect your input graph file:\n");
 					printf("\n\t0- agrocyc_dag_uniq");
 					printf("\n\t1- amaze_dag_uniq");
 					printf("\n\t2- anthra_dag_uniq");
@@ -123,6 +127,7 @@ int main(int argc, char** argv) {
 					printf("\n\t7- nasa_dag_uniq");
 					printf("\n\t8- vchocyc_dag_uniq");
 					printf("\n\t9- xmark_dag_uniq\n");
+					printf("\n\n----> Make your choice: ");
 					scanf_s("%d", &choice);
 					switch (choice) {
 					case 0:
@@ -173,19 +178,25 @@ int main(int argc, char** argv) {
 			case large:
 				do {
 					printf("\nOk,let's use  one of large real graphs\n");
-					printf("\nSelect your input graph file:");
-					printf("\n\t0- cit-Patents.scc.gra");
-					printf("\n\t1- unipretenc_22m.scc.gra");
+					printf("\nSelect your input graph file:\n");
+					printf("\n\t0- unipretenc_22m.scc.gra");
+					printf("\n\t1- cit-Patents.scc.gra");
+					printf("\n\n----> Make your choice: ");
 					scanf_s("%d", &choice);
 					switch (choice) {
+
 					case 0:
-						swprintf_s(graphInput, _T("./benchmark/large_real/cit-Patents.scc.gra"));
-						swprintf_s(queryInput, _T("./benchmark/large_real/cit-Patents.scc.que"));
-						break;
-					case 1:
 						swprintf_s(graphInput, _T("./benchmark/large_real/unipretenc_22m.scc.gra"));
 						swprintf_s(queryInput, _T("./benchmark/large_real/unipretenc_22m.scc.que"));
 						break;
+					
+						
+					case 1:
+						swprintf_s(graphInput, _T("./benchmark/large_real/cit-Patents.scc.gra"));
+						swprintf_s(queryInput, _T("./benchmark/large_real/cit-Patents.scc.que"));
+						break;
+						
+					
 					default:
 						printf("\nSorry, I cannot understand what you want to do. Retry\n");
 					}
@@ -250,7 +261,9 @@ int main(int argc, char** argv) {
 			swprintf_s(prg, _T("%s %s %d %s"), nameExeMem, graphInput, labels, queryInput);
 		else
 			swprintf_s(prg, _T("%s %s %d %s"), nameExeFile, graphInput, labels, queryInput);
-
+		
+		wprintf_s(_T("%s\n"), prg);
+		
 		ZeroMemory(&si, sizeof(si));
 		si.cb = sizeof(si);
 		ZeroMemory(&pi, sizeof(pi));
